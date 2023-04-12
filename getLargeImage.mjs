b9aa7e9168eaf56e69b07b0a51947652946d36d7
kernoeb <kernoeb@protonmail.com>
2023-04-12 15:06:26 +0200

    chore: clean code + lint

--- getLargeImage.mjs
+++ getLargeImage.mjs
@@ -1,12 +1,33 @@
-import maximage from './userscript.user.js'
+import maxImage from './userscript.user.js'
 import iconv from 'iconv-lite'
 import request from 'request'
 
 const jar = request.jar()
 
-export default async (smallimage) => {
+const headers = {
+  'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36',
+  pragma: 'no-cache',
+  'cache-control': 'max-age=0',
+  accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+  'accept-encoding': 'gzip, deflate',
+  'accept-language': 'en-US,en;q=0.9'
+}
+
+/**
+ * Fix headers, remove empty values, lowercase keys
+ * @param {object} headers
+ */
+const fixHeaders = (headers) => {
+  Object.keys(headers).forEach(key => {
+    if (!headers[key]) delete headers[key]
+    headers[key.toLowerCase()] = headers[key]
+    delete headers[key]
+  })
+}
+
+export default async (smallImage) => {
   return await new Promise((resolve, reject) => {
-    maximage(smallimage, {
+    maxImage(smallImage, {
       fill_object: true,
       iterations: 200,
       use_cache: true,
@@ -15,31 +36,12 @@ export default async (smallimage) => {
       include_pastobjs: true,
       force_page: false,
       allow_thirdparty: false,
-      filter: function () {
-        return true
-      },
-      do_request: function (options) {
-        const headers = {
-          'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36',
-          pragma: 'no-cache',
-          'cache-control': 'max-age=0',
-          accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-          'accept-encoding': 'gzip, deflate',
-          'accept-language': 'en-US,en;q=0.9'
-        }
-
-        if (options.headers) {
-          for (const header in options.headers) {
-            const headername = header.toLowerCase()
-            const value = options.headers[headername]
-            if (value) headers[headername] = value
-            else delete headers[headername]
-          }
-        }
+      filter: () => true,
+      do_request: options => {
+        if (options.headers) fixHeaders(options.headers)
 
         console.log('Requesting ', options.url)
-
-        const requestopts = {
+        const requestOpts = {
           method: options.method,
           uri: options.url,
           jar,
@@ -49,57 +51,32 @@ export default async (smallimage) => {
           encoding: null
         }
 
-        if (options.data) {
-          requestopts.body = options.data
-        }
+        if (options.data) requestOpts.body = options.data
 
-        request(requestopts, function (error, response, body) {
-          if (error) {
-            console.error(error)
-          // console.log(requestopts);
-          }
-
-          if (!response) {
-            console.error('Unable to get response')
-            return
-          }
-
-          let loc = response.caseless.get('location')
-          if (!loc) loc = response.request.href
+        request(requestOpts, (error, response, body) => {
+          if (error) console.error(error)
+          if (!response) return reject(new Error('No response'))
 
           let encoding = 'utf8'
           if (options.overrideMimeType) {
-            const charsetmatch = options.overrideMimeType.match(/;\s*charset=([^;]*)/)
-            if (charsetmatch) {
-              encoding = charsetmatch[1]
-            }
+            const charsetMatch = options.overrideMimeType.match(/;\s*charset=([^;]*)/)
+            if (charsetMatch) encoding = charsetMatch[1]
           }
-
-          body = iconv.decode(body, encoding)
 
           const resp = {
             readyState: 4,
-            finalUrl: loc,
-            responseText: body,
+            finalUrl: response.caseless.get('location') || response.request.href,
+            responseText: iconv.decode(body, encoding),
             status: response.statusCode,
             statusText: response.statusMessage
           }
 
           options.onload(resp)
         })
-      },
-      // Callback
-      cb: function (result) {
-        if (!result) {
-          console.log('No result')
-          return reject(new Error('No result'))
-        }
-
-        if (result.length === 1 && result[0].url === smallimage) {
-          console.log('No larger image was found')
-          // No larger image was found
-          return reject(new Error('No larger image was found'))
-        }
+      }, // Callback
+      cb: result => {
+        if (!result) return reject(new Error('No result'))
+        if (result.length === 1 && result[0].url === smallImage) return reject(new Error('No larger image was found'))
 
         try {
           return resolve(result.find(v => !v.is_pagelink).url)
